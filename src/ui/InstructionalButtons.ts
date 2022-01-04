da8f65f685d19350519cfd087919818115cd66d7
Liam Dormon <66041893+Mojito-Fivem@users.noreply.github.com>
2022-01-04 19:19:42 +0000

    feat(ui/instructional-buttons): Allow multiple controls assigned to one label (#5)

--- src/ui/InstructionalButtons.ts
+++ src/ui/InstructionalButtons.ts
@@ -8,8 +8,8 @@ import { IButton } from './interfaces/IButton';
  * import { InstructionalButtons, Control } from '@nativewrappers/client';
  *
  * const buttons = new InstructionalButtons([
- *  {control: Control.Context, label: "Interact with Bob"},
- *  {control: Control.Detonate, label: "Say hello to Alice"}
+ *  {controls: [Control.Context], label: "Interact with Bob"},
+ *  {controls: [Control.Detonate], label: "Say hello to Alice"}
  * ])
  *
  * setTick(() => {
@@ -19,7 +19,6 @@ import { IButton } from './interfaces/IButton';
  */
 export class InstructionalButtons {
   private scaleform: Scaleform;
-  private counter = 0;
 
   /**
    * Draws native instructional buttons
@@ -31,22 +30,26 @@ export class InstructionalButtons {
     this.scaleform.callFunction('CLEAR_ALL');
     this.scaleform.callFunction('SET_CLEAR_SPACE', 200);
 
-    buttons.forEach(button => {
-      this.pushButton(button);
+    buttons.forEach((button, index) => {
+      this.pushButton(button, index);
     });
 
     this.scaleform.callFunction('DRAW_INSTRUCTIONAL_BUTTONS');
     this.scaleform.callFunction('SET_BACKGROUND_COLOUR', 0, 0, 0, 80);
   }
 
-  private pushButton(button: IButton) {
-    this.scaleform.callFunction(
-      'SET_DATA_SLOT',
-      this.counter,
-      GetControlInstructionalButton(2, button.control, true),
-      button.label,
-    );
-    this.counter++;
+  private pushButton(button: IButton, index: number) {
+    BeginScaleformMovieMethod(this.scaleform.Handle, 'SET_DATA_SLOT');
+    PushScaleformMovieFunctionParameterInt(index);
+    // Looping backwards here since scaleform is using a stack so the first control ends up being the last
+    // So looping backwards makes more sense here so that the controls are rendered in the order they're defined
+    for (let i = button.controls.length - 1; i >= 0; i--) {
+      PushScaleformMovieMethodParameterButtonName(
+        GetControlInstructionalButton(2, button.controls[i], true),
+      );
+    }
+    PushScaleformMovieMethodParameterString(button.label);
+    EndScaleformMovieMethod();
   }
 
   public async draw(): Promise<void> {
