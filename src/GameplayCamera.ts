9569594f592f3af2fddd234271c5c9a79b46420c
MythicalBro <artuurs.kupcs@gmail.com>
2022-07-12 23:06:57 +0300

    feat(camera): set shakeNames property visibility to public static, add new gameplay camera get/set methods, add camera handle public getter method

--- src/GameplayCamera.ts
+++ src/GameplayCamera.ts
@@ -1,3 +1,5 @@
+import { Camera } from './Camera';
+import { CameraShake } from './enums';
 import { Vector3 } from './utils';
 
 /**
@@ -58,5 +60,108 @@ export abstract class GameplayCamera {
    */
   public static set RelativeHeading(heading: number) {
     SetGameplayCamRelativeHeading(heading);
+  }
+
+  /**
+   * Clamps the yaw of the gameplay camera.
+   *
+   * @param min The minimum yaw value.
+   * @param max The maximum yaw value.
+   */
+  public static clampYaw(min: number, max: number): void {
+    ClampGameplayCamYaw(min, max);
+  }
+
+  /**
+   * Clamps the pitch of the gameplay camera.
+   *
+   * @param min The minimum pitch value.
+   * @param max The maximum pitch value.
+   */
+  public static clampPitch(min: number, max: number): void {
+    ClampGameplayCamPitch(min, max);
+  }
+
+  /**
+   * Gets zoom of the gameplay camera.
+   */
+  public static get Zoom(): number {
+    return GetGameplayCamZoom();
+  }
+
+  /**
+   * Gets field of view of the gameplay camera.
+   */
+  public static get FieldOfView(): number {
+    return GetGameplayCamFov();
+  }
+
+  /**
+   * Gets a value indicating whether the gameplay camera is rendering.
+   *
+   * @returns true if the gameplay camera is rendering; otherwise, false.
+   */
+  public static get IsRendering(): boolean {
+    return IsGameplayCamRendering();
+  }
+
+  /**
+   * Gets a value indicating whether the aiming camera is rendering.
+   *
+   * @returns true if the aiming camera is rendering; otherwise, false.
+   */
+  public static get IsAimCamActive(): boolean {
+    return IsAimCamActive();
+  }
+
+  /**
+   * Gets a value indicating whether the first person aiming camera is rendering.
+   *
+   * @returns true if the first person aiming camera is rendering; otherwise, false.
+   */
+  public static get IsFirstPersonAimCamActive(): boolean {
+    return IsFirstPersonAimCamActive();
+  }
+
+  /**
+   * Gets a value indicating whether the gameplay camera is looking behind.
+   *
+   * @returns true if the gameplay camera is looking behind; otherwise, false.
+   */
+  public static get IsLookingBehind(): boolean {
+    return IsGameplayCamLookingBehind();
+  }
+
+  /**
+   * Shakes the gameplay camera.
+   *
+   * @param shakeType Type of the shake to apply.
+   * @param amplitude The amplitude of the shaking.
+   */
+  public static shake(shakeType: CameraShake, amplitude: number): void {
+    ShakeGameplayCam(Camera.shakeNames[Number(shakeType)], amplitude);
+  }
+
+  /**
+   * Stops shaking the gameplay camera.
+   */
+  public static stopShaking(): void {
+    StopGameplayCamShaking(true);
+  }
+
+  /**
+   * Gets a value indicating whether the gameplay camera is shaking.
+   *
+   * @returns true if the gameplay camera is shaking; otherwise, false.
+   */
+  public static get IsShaking(): boolean {
+    return IsGameplayCamShaking();
+  }
+
+  /**
+   * Sets the shake amplitude for the gameplay camera.
+   */
+  public static set ShakeAmplitude(value: number) {
+    SetGameplayCamShakeAmplitude(value);
   }
 }
