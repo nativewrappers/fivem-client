d362ad80ea3d196eb93bbdaccf3ff7c030fcbd1f
Dillon Skaggs <dillskaggs@gmail.com>
2022-11-28 02:06:17 -0600

    feat(EntityBone): add rotation getter

--- src/models/EntityBone.ts
+++ src/models/EntityBone.ts
@@ -2,29 +2,33 @@ import { Vector3 } from '../utils';
 import { Entity } from './';
 
 export class EntityBone {
-	public get Index(): number {
-		return this.index;
-	}
+    public get Index(): number {
+        return this.index;
+    }
 
-	public get Owner(): Entity {
-		return this.owner;
-	}
+    public get Owner(): Entity {
+        return this.owner;
+    }
 
-	public get Position(): Vector3 {
-		return Vector3.fromArray(GetWorldPositionOfEntityBone(this.owner.Handle, this.index));
-	}
+    public get Position(): Vector3 {
+        return Vector3.fromArray(GetWorldPositionOfEntityBone(this.owner.Handle, this.index));
+    }
 
-	public get IsValid(): boolean {
-		return this.owner.exists() && this.index !== -1;
-	}
+    public get Rotation(): Vector3 {
+        return Vector3.fromArray(GetEntityBoneRotation(this.owner.Handle, this.index));
+    }
 
-	protected readonly owner: Entity;
-	protected readonly index: number;
+    public get IsValid(): boolean {
+        return this.owner.exists() && this.index !== -1;
+    }
 
-	constructor(owner: Entity, boneIndex?: number, boneName?: string) {
-		this.owner = owner;
-		this.index = boneIndex
-			? boneIndex
-			: GetEntityBoneIndexByName(this.owner.Handle, boneName ?? '');
-	}
+    protected readonly owner: Entity;
+    protected readonly index: number;
+
+    constructor(owner: Entity, boneIndex?: number, boneName?: string) {
+        this.owner = owner;
+        this.index = boneIndex
+            ? boneIndex
+            : GetEntityBoneIndexByName(this.owner.Handle, boneName ?? '');
+    }
 }
